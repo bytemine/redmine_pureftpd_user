d3cbe3f6800372bcfac2e3974da35b511cd74c9c
Bernd Ahlers <ahlers@bytemine.net>
2009-05-26 18:27:20 +0200

    Implement the PureftpdUser model.

--- app/models/pureftpd_user.rb
+++ app/models/pureftpd_user.rb
@@ -1,2 +1,25 @@
 class PureftpdUser < ActiveRecord::Base
+  def self.update_user(user)
+    puser = self.find_by_user(user.login) || self.new(:user => user.login)
+    puser.password = encrypt_password(user.password)
+    puser.save
+  end
+
+  private
+    def self.encrypt_password(password)
+      password.crypt(create_salt)
+    end
+
+    def self.create_salt
+      chars = ['.', '/', '0'..'9', 'A'..'Z', 'a'..'z'].collect do |char|
+        char.to_a
+      end
+      chars = chars.flatten
+
+      salt = ""
+      2.times do
+        salt << chars[rand(chars.length)]
+      end
+      salt
+    end
 end
