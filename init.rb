b820ceedf0d7705a41e835d109ee70d461c31a83
Bernd Ahlers <ahlers@bytemine.net>
2009-05-26 20:46:38 +0200

    Bump to version 1.0.

--- init.rb
+++ init.rb
@@ -10,5 +10,5 @@ Redmine::Plugin.register :redmine_pureftpd_user do
   name 'Redmine Pureftpd User plugin'
   author 'Bernd Ahlers'
   description 'This plugin maintains a table with pureftpd compatible users.'
-  version '0.9'
+  version '1.0'
 end
