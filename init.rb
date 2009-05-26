b0cece6fda3e93f48cc26f0c7f806f4e616cd256
Bernd Ahlers <ahlers@bytemine.net>
2009-05-26 18:23:29 +0200

    Add a description, my name and a version number.

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_pureftpd_user do
   name 'Redmine Pureftpd User plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
+  author 'Bernd Ahlers'
+  description 'This plugin maintains a table with pureftpd compatible users.'
+  version '0.9'
 end
