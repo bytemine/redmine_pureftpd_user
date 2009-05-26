fcafb647319bf349278f47057b2f50ce28ccd3ad
Bernd Ahlers <ahlers@bytemine.net>
2009-05-26 18:21:09 +0200

    Import empty redmine plugin.

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_pureftpd_user do
+  name 'Redmine Pureftpd User plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
